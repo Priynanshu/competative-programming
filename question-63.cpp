da160850861078e7fa21dc66615e7ccaf1404a4b
Priyanshu Kumar <priyanshu3960@gmail.com>
2026-03-29 00:27:54 +0530

    day 55

---
+++ question-63.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    while (s.find("WUB") != string::npos)
+    {
+        s.replace(s.find("WUB"), 3, " ");
+    }
+
+    cout<<s;
+
+    return 0;
+}
