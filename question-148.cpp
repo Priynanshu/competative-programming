b64a44d240a65617443b25524c629813b6c4c9dd
Priyanshu Kumar <priyanshu3960@gmail.com>
2026-08-05 12:49:58 +0530

    Add solution for problem 148: implement logic to determine player turn based on input values

---
+++ question-148.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, r;
+    cin>>a >>b>>r;
+
+    if(a < 2*r || b < 2*r) {
+        cout<<"Second"<<endl;
+    }else {
+        cout<<"First"<<endl;
+    }
+
+    return 0;
+}
