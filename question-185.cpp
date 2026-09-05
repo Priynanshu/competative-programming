662cfd29f501ad6f0e45a01a39c141406e0d5bad
Priyanshu Kumar <priyanshu3960@gmail.com>
2026-09-05 16:51:16 +0530

    Add solution for problem 185: implement logic to count minimum elements needed to reach or exceed a target sum

---
+++ question-185.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, t;
+    cin >> n >> t;
+
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) cin >> a[i];
+
+    int left = 0;
+    long long sum = 0;
+    int ans = 0;
+
+    for(int right = 0; right < n; right++) {
+        sum += a[right];
+
+        while(sum > t) {
+            sum -= a[left];
+            left++;
+        }
+
+        ans = max(ans, right - left + 1);
+    }
+
+    cout << ans << endl;
+}
